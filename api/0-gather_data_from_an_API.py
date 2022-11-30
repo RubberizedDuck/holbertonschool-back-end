eb76429bdce590bf8483f31ad4512bdf534a9591
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-30 16:00:56 +1100

    Add: few lines to start working in linux

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,7 +5,11 @@
 
 import requests
 import json
-response_API = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+
+def get_data(user_id):
+    """ retrieves data from an api """
+    user_list = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    todo_list = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
 data = response_API.text
 #print(response_API)
 parse_json = json.loads(data)

cab2ff6cac96d02dc10ccf0ef1d5094102055e0d
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-01 01:26:23 +0000

    Exports api data to json file

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module is designed to extract data from an API
+and export to a CSV file
+"""
+
+
+import csv
+import json
+import requests
+from sys import argv
+
+
+def export_csv_from_api(user_id):
+    """ retrieves data from an api to export in csv """
+    # setting variable for users and their todos with html of API
+    user_list = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    user_todo = 'https://jsonplaceholder.typicode.com/users/{}/todos'\
+        .format(user_id)
+
+    # using requests to get information and converting to json format
+    user_info = requests.get(user_list).json()
+    todo_info = requests.get(user_todo).json()
+    json_dict = {user_id: []}
+
+    for data in todo_info:
+        if data['userId'] == int(user_id):
+            row = {"task": data['title'],
+                   "completed": data['completed'],
+                   "username": user_info['username']}
+        json_dict[user_id].append(row)
+    with open("{}.json".format(user_id), "w") as f:
+        json.dump(json_dict, f)
+
+# using the second argument after the file as the user id
+if __name__ == "__main__":
+    export_csv_from_api(int(argv[1]))
